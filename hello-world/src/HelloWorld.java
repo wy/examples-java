2038cd3c3efd0749909d4a6a00edfd82606c2a82
Wing Yung Chan <wy@users.noreply.github.com>
2017-08-30 20:56:14 +0100

    Needs to be capital M

--- hello-world/src/HelloWorld.java
+++ hello-world/src/HelloWorld.java
@@ -2,7 +2,7 @@ import AntShares.SmartContract.Framework.Services.AntShares.Storage;
 
 public class HelloWorld {
 
-  public static void main(String[] args){
+  public static void Main(String[] args){
     Storage.Put(Storage.getCurrentContext(), "Greeting to the World", "Hello World!");
   }
 
