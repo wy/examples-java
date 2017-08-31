bd066607d693524e4f1ae279a40b52cc4b21e78a
Wing Yung Chan <wy@users.noreply.github.com>
2017-08-31 21:05:49 +0100

    bytetest that is currently failing compiler

---
+++ bytetest.java
@@ -0,0 +1,14 @@
+import AntShares.SmartContract.Framework.FunctionCode;
+
+public class bytetest extends FunctionCode{
+
+    public static void Main() {
+
+        byte[] lllwvlvwlll = new byte[]{(byte) 3, (byte) 84, (byte) 174, (byte) 73, (byte) 130, (byte) 33, (byte) 4,
+                (byte) 108, (byte) 102, (byte) 110, (byte) 254, (byte) 187, (byte) 174, (byte) 233, (byte) 189,
+                (byte) 14, (byte) 180, (byte) 130, (byte) 52, (byte) 105, (byte) 201, (byte) 142, (byte) 116,
+                (byte) 132, (byte) 148, (byte) 169, (byte) 42, (byte) 113, (byte) 243, (byte) 70, (byte) 177,
+                (byte) 166, (byte) 97};
+
+    }
+}
